cf676af2898f46e718a7b1886cd22379d7fcc261
Roman <romanvoitetckii@gmail.com>
2021-09-02 12:44:46 +0500

    ex is done. still there are some memory leaks.

--- OS_Principles_and_practices/chapter 4/ex5/main.c
+++ OS_Principles_and_practices/chapter 4/ex5/main.c
@@ -5,62 +5,105 @@
 #include <stdlib.h>
 
 
-#define NUM 10
-int step_i = 0;
-
-int matrix1[10][10];
-int matrix2[10][10];
-int result[10][10];
+#define NUM 4
 /* */
 void* multi(void* arg);
-//static thread_t threads[NUM];
-
-static pthread_t threads[NUM];
-static pthread_attr_t attrs[NUM];
-
 
 
 int main(int argc, char **argv){
     srand(time(NULL));
-    int num = 10;
+    int matrix1[NUM][NUM];
+    int matrix2[NUM][NUM];
 
-    for (int i = 0; i < 10; i++)
+    int r1 = NUM, c1 = NUM, r2 = NUM, c2 = NUM;
+    int i,j,k;
+
+    for (int i = 0; i < r1; i++)
     {
-        for (int j = 0; j < 10; j++){
+        for (int j = 0; j < c1; j++){
             matrix1[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++){
             matrix2[i][j] = rand() % 10;
         }
     }
 
-    for (int i = 0; i < 10; i++)
-    {
-        pthread_attr_init(&attrs[i]);
-        pthread_create(&(threads[i]),&attrs[i], multi, &num);
-    }
-    
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
-            printf("%d ", result[i][j]);
-        }
+    /* Displaying Matrix 1 */
+    for (i = 0; i < r1; i++){
+        for(j = 0; j < c1; j++)
+            printf("%d ",matrix1[i][j]);
         printf("\n");
     }
 
-    thread_join((threads[10]));
+    printf("-----------------------------\n");
+
+    /* Displaying Matrix 2 */
+    for (i = 0; i < r1; i++){
+        for(j = 0; j < c1; j++)
+            printf("%d ",matrix1[i][j]);
+        printf("\n");
+    }
+
+    pthread_t *threads;
+    threads = (pthread_t*)malloc(c1*r2*sizeof(pthread_t));
+
+    int count = 0;
+    int *data = NULL;
+
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++){
+            data = (int *)malloc((20)*sizeof(int));
+            data[0]=c1;
+
+            for (k = 0; k < c1; k++)
+            {
+                data[k+1] = matrix1[i][k];
+            }
+
+            for (k = 0; k < c1; k++)
+            {
+                data[k+c1+1] = matrix2[k][j];
+            }
+            pthread_create(&threads[count++], NULL, multi, (void*)(data));
+            
+        }
+    }
+    
+    printf("Result : \n");
+
+    for(int i = 0; i < NUM * NUM; i++){
+        void *k;
+        pthread_join(threads[i], &k);
+
+        int *p = (int *)k;
+        printf("%d ", *p);
+        if((i+1)%c2 == 0) printf("\n");
+    }
+
+    free(threads);
 
     return 0;
 }
 
-void* multi(void* arg){
-    int i = step_i;
+void *multi(void* arg){
 
-    int count = *(int*)arg;
+    int *data = (int*)arg;
+    int k = 0, i = 0;
 
-    for (int j = 0; i < count; j++)
+    int x = data[0];
+
+    for (i = 1; i <= x; i++)
     {
-        for (int k = 0; k < count; k++)
-        {
-            result[i][j] += matrix1[i][k] * matrix2[k][j];
-        }   
+        k+=data[i]*data[i+x];
     }
-    return 0;
+
+    int *p = (int*)malloc(sizeof(int));
+    *p = k;
+    
+    pthread_exit(p);
 }
